7d9da6c6ff4839e436a81e59d90403ca2ba7e39b
Ali-Hasan3312 <alihasan331229@gmail.com>
2024-08-19 10:23:13 +0500

    add hover effect on navbar

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,15 +7,13 @@ interface heroCountProps {
 const Background = ({stateValue}:heroCountProps) => {
    
   if(stateValue===0){
-    return <img className="fixed inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0"  src={image1} />
+    return <img className="inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0"  src={image1} />
   }else if(stateValue===1){
-    return <img className="fixed inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0"  src={image2} />
+    return <img className="inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0"  src={image2} />
   }else if(stateValue===2){
-    return <img  className="fixed inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0" src={image3} />
+    return <img  className="inset-x-0 top-[190px] w-full h-[500px] object-cover z-[-1] p-0" src={image3} />
   }
-  return (
-    <div>Background</div>
-  )
+ 
 }
 
 export default Background
